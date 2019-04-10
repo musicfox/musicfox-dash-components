b9cebda3e3ce0e980999b986c58ee04e8e68f51d
Jason R. Stevens <thinkjrs@gmail.com>
2019-04-09 21:48:21 -0500

    Add initial boilerplate for project setup

---
+++ musicfox_dash_components/musicfox_dash_components/_imports_.py
@@ -0,0 +1,5 @@
+from .Hello import Hello
+
+__all__ = [
+    "Hello"
+]
---
+++ musicfox_dash_components/src/lib/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+import Hello from './components/Hello.react';
+import HelloTwo from '/components/HelloTwo.react';
+export {
+    Hello
+};
+export {
+    HelloTwo
+};
