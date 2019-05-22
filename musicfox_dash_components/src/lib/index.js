08e416f2dcc3834fcf4798dbae4070ef0c0bb4be
Jason R. Stevens <thinkjrs@gmail.com>
2019-05-22 14:26:47 -0500

    update w/ToDo.js export and import

--- musicfox_dash_components/src/lib/index.js
+++ musicfox_dash_components/src/lib/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import Hello from './components/Hello.react';
+import ToDo from './components/ToDo.react';
 export {
-    Hello
+    Hello,
+    ToDo
 };
